dd8dae3cc8da836a46c79e6808c8f71294e5b5da
张煜东 <bgtcrhm@163.com>
2024-01-26 10:39:42 +0800

    查询测评分数页面

--- src/services/score.ts
+++ src/services/score.ts
@@ -1,14 +1,37 @@
 import { http } from "@/utils/http";
 
 /**
- * 
- * @param data 
+ *
+ * @param data
  * 添加测评
  */
 export const addEval = (data: any) => {
-    return http({
-      method: "POST",
-      url: "/system/eval",
-      data,
-    });
-  };
+  return http({
+    method: "POST",
+    url: "/system/eval",
+    data,
+  });
+};
+
+//获取测评列表
+export const getEvalList = () => {
+  return http({
+    method: "GET",
+    url: "/system/eval/list",
+  });
+};
+
+export const getScore = (rid: any) => {
+  return http({
+    method: "POST",
+    url: "/system/evaldetails/avgscop?rid=" + rid,
+  });
+};
+
+export const saveScore = (data: any) => {
+  return http({
+    method: "PUT",
+    data,
+    url: "/system/eval",
+  });
+};
